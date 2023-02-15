acae88d96c089dbd74b554f19e2e11c71d8ff3c5
David Ospina <J_do_o13@hotmail.com>
2023-02-15 14:45:41 -0500

    Init
    creación de todas las variables y métodos iniciales que se usarán durante el proceso

--- main.js
+++ main.js
@@ -1,10 +1,72 @@
 const { createApp } = Vue;
 createApp({
     data() {
-        return {     }
+        return {
+            Log: 0,
+            Userl: '',
+            Passl: '',
+            Users: [],
+            Name: '',
+            Email: '',
+            Number: '',
+            User: [],
+            Cards: [],
+            Shop: [{
+                    Price: 10000,
+                    Amount: 100,
+                   },{
+                    Price: 20000,
+                    Amount: 210,
+                   },{
+                    Price: 50000,
+                    Amount: 650,
+                   },{
+                    Price: 100000,
+                    Amount: 1500,
+                   },],
+            RM: 0,
+        }
     },
-    methods: { },
+    methods: {
+        Login(){
+
+        },
+        Register(){
+
+        },        
+        Cart(){
+
+        },        
+        Logout(){
+
+        },        
+        RandomPrice(){
+
+        },        
+        
+        RandomType(){
+
+        },       
+        
+        ShopRM(){
+
+        },
+        
+        BuyRP(){
+
+        },
+        async ResultsC() {
+            await fetch('https://rickandmortyapi.com/api/character/?page=' + this.Page)
+                .then((response) => response.json())
+                .then((data) => this.Cards = data.results)
+            localStorage.setItem('Cards', JSON.stringify(this.Cards))
+        },
+    },
     mounted() {
     },
-    created() {},
+    created() {        
+        (localStorage.getItem('Cards') == null) ?
+            this.ResultsC() :
+            this.Cards = JSON.parse(localStorage.getItem('Cards'));
+    },
 }).mount("#root");
